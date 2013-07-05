fa903cb962f3305a051be56e202e6765e8f176d0
Gabor Toldi <gabor.toldi@gmail.com>
2013-07-05 11:04:17 +0200

    Modified web.js: hello message from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var msg =  fs.readFileSync("index.html", "utf-8");
+	response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
